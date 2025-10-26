143483006faa925efe5779c608709eaf8256e166
Ricky <thechaosmyth7@gmail.com>
2025-10-26 23:06:15 +0700

    Add GitHub Pages auto-deploy workflow

--- app/app.vue
+++ app/app.vue
@@ -1,46 +1,28 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
-const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
+const color = computed(() =>
+  colorMode.value === "dark" ? "#020618" : "white",
+);
 
 useHead({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color }
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
   ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
 useSeoMeta({
-  titleTemplate: '%s - Nuxt Portfolio Template',
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterCard: 'summary_large_image'
-})
-
-const [{ data: navigation }, { data: files }] = await Promise.all([
-  useAsyncData('navigation', () => {
-    return Promise.all([
-      queryCollectionNavigation('blog')
-    ])
-  }, {
-    transform: data => data.flat()
-  }),
-  useLazyAsyncData('search', () => {
-    return Promise.all([
-      queryCollectionSearchSections('blog')
-    ])
-  }, {
-    server: false,
-    transform: data => data.flat()
-  })
-])
+  titleTemplate: "Ricky Ng - Profile",
+  ogImage: "https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png",
+  twitterImage: "https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
@@ -50,15 +32,5 @@ const [{ data: navigation }, { data: files }] = await Promise.all([
         <NuxtPage />
       </UMain>
     </NuxtLayout>
-
-    <ClientOnly>
-      <LazyUContentSearch
-        :files="files"
-        :navigation="navigation"
-        shortcut="meta_k"
-        :links="navLinks"
-        :fuse="{ resultLimit: 42 }"
-      />
-    </ClientOnly>
   </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,42 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/content',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
-    'motion-v/nuxt'
-  ],
+  ssr: true,
 
-  devtools: {
-    enabled: true
+  app: {
+    baseURL: "/", // ✅ karena ini user site, bukan project site
   },
 
-  css: ['~/assets/css/main.css'],
-
-  compatibilityDate: '2024-11-01',
-
   nitro: {
+    preset: "github-pages",
     prerender: {
-      routes: [
-        '/'
-      ],
-      crawlLinks: true
-    }
+      routes: ["/"],
+      crawlLinks: true,
+    },
+  },
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    "motion-v/nuxt",
+  ],
+
+  css: ["~/assets/css/main.css"],
+
+  compatibilityDate: "2024-11-01",
+
+  devtools: {
+    enabled: true,
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
